64e2ee772a91ab1b3dcc6c70bed7b832a15c1a2d
wesdoyle <wesdoyle08@gmail.com>
2023-08-16 15:32:38 -0500

    Use correct filenames after benchmarking

--- bagit_modules/filenames.py
+++ bagit_modules/filenames.py
@@ -1,6 +1,6 @@
-def encode_filename_chain(s):
+def encode_filename(s):
     return s.replace("\r", "%0D").replace("\n", "%0A")
 
 
-def decode_filename_replace(s):
+def decode_filename(s):
     return s.replace("%0D", "\r").replace("%0A", "\n").replace("%0d", "\r").replace("%0a", "\n")
